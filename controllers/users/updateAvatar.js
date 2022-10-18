8b6f7d3956632ee957597362bb8b62b5fa3a58ce
Yevhen Podoliaka <zanp85740@gmail.com>
2022-10-18 18:24:47 +0300

    add jimp and method Jipm.resize avatar

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,15 +1,15 @@
-const path = require("path")
-const fs=require("fs/promises")
+const path = require("path");
+const fs = require("fs/promises");
 const { User } = require("../../models");
-
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tmpUpload, originalname } = req.file;
-  const avatarName= `${_id}_${originalname}`
-  
+  const avatarName = `${_id}_${originalname}`;
+
   try {
     const resultUpload = path.join(avatarsDir, avatarName);
     await fs.rename(tmpUpload, resultUpload);
@@ -19,23 +19,29 @@ const updateAvatar = async (req, res) => {
       { avatarURL },
       { runValidators: true }
     );
-     res.json({
-       status: "success",
-       code: 200,
-       data: { avatarURL },
-     });
+
+    Jimp.read(`${avatarsDir}/${avatarName}`)
+      .then((image) => {
+        return image.resize(256, 256).write(`${avatarsDir}/${avatarName}`);
+      })
+      .catch((err) => {
+        throw err;
+      });
+    res.json({
+      status: "success",
+      code: 200,
+      data: { avatarURL },
+    });
     if (!updateUser) {
       const error = new Error(`user with id=${_id} not found`);
       error.status = 404;
       throw error;
     }
   } catch (error) {
-    await fs.unlink()
+    await fs.unlink();
     error.status = 400;
     throw error;
   }
-
- 
 };
 
 module.exports = updateAvatar;
